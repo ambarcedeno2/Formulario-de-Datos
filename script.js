9862a28253cfcc71ea87ef7564eea61f359512db
ambarcedeno2 <137855484+ambarcedeno2@users.noreply.github.com>
2024-04-25 11:08:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript
+document.getElementById('clienteForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita el envío del formulario por defecto
+  
+  // Aquí puedes agregar la lógica para procesar los datos del formulario
+});
