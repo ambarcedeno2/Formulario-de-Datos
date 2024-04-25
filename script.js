cabf9417729c9d26e2c08fc2665eea2fe30aaba0
ambarcedeno2 <137855484+ambarcedeno2@users.noreply.github.com>
2024-04-25 11:12:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,4 @@
 document.getElementById('clienteForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Evita el envío del formulario por defecto
   
-  // Aquí puedes agregar la lógica para procesar los datos del formulario
 });
